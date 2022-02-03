1c6579eeb4b822c815a03e8fda8578aa955eda68
Joerg Thomas <j1981thomas@gmail.com>
2022-02-03 09:28:36 +0100

    Projektdateien hinzufügen.

---
+++ MVVM_WPF/App.xaml.cs
@@ -0,0 +1,20 @@
+﻿using MVVM_WPF.View;
+using MVVM_WPF.ViewModel;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace MVVM_WPF
+{
+    /// <summary>
+    /// Interaktionslogik für "App.xaml"
+    /// </summary>
+    public partial class App : Application
+    {
+       
+    }
+}
